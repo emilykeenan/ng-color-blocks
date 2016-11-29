b785c82c6a3d8d90950f22767985a59dc03b33db
emilykeenan <emilyjanekeenan@gmail.com>
2016-11-28 20:18:18 -0600

    basic mode done

--- server/public/scripts/controllers/settingsController.js
+++ server/public/scripts/controllers/settingsController.js
@@ -1,5 +1,18 @@
-colorBlocks.controller('SettingsController', ['$scope', function($scope) {
+colorBlocks.controller('SettingsController', ['$scope', "DataFactory", function($scope, DataFactory) {
   console.log("settings controller running");
 
-  
+  var self = this;
+  self.colors = DataFactory.colors;
+  self.newColor = '';
+
+console.log(self.colors);
+
+  self.addColor = function() {
+    console.log(self.newColor);
+    self.colors.push(self.newColor);
+    DataFactory.colors = self.colors;
+    self.newColor = '';
+  }
+
+
 }]);
